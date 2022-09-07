0693d5249d47fc68a9d8cb1cfedebb5bb75a5dd3
marcoby <vjackson601@gmail.com>
2022-09-07 14:58:10 -0700

    Initial commit

---
+++ Okta/okta-prefs.js
@@ -0,0 +1,3 @@
+// Don't skip this comment. If the first line is not a comment, the file will not load.
+pref("general.config.obscure_value", 0); // Don't use ROT13 encoding for the config file.
+pref("general.config.filename", "okta-mozilla.cfg"); // Tell Firefox to load the okta-mozilla.cfg config file.
